1f65ca70987d8ddcbfc77c12d0ddc1d291cd5882
Jake Lewis <jakeaustinlewis@gmail.com>
2018-05-20 16:19:03 -0500

    removed unnecessary lines

--- script.js
+++ script.js
@@ -2,18 +2,17 @@
 
 
 document.addEventListener("DOMContentLoaded", function (event) {
-    let x = document.createElement("div");
-    // x.textContent = 'We hope you enjoyed the ride.';
-    x.style.cssText = "background-color: red";
 
     function createCheckerboard() {
         for (var i = 0; i < 63; i++) {
             let square = document.createElement("div");
+            // create red squares
             if (i % 2 == 0) {
                 square.style.cssText = "background-color: red; width: 11.1%; float: left; padding-bottom: 11.1%";
                 square.textContent = i;
                 document.body.appendChild(square);
-                //remove all children and then repoluate
+            
+            // create black squares
             } else {
                 square.style.cssText = "background-color: black; width: 11.1%; float: left; padding-bottom: 11.1%";
                 square.textContent = i;
@@ -24,3 +23,5 @@ document.addEventListener("DOMContentLoaded", function (event) {
     }
     createCheckerboard();
 });
+
+ 
