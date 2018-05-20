4531c18f1f4d1075f7c8b016046065b1cd04b590
Jake Lewis <jakeaustinlewis@gmail.com>
2018-05-20 16:45:22 -0500

    checkerboard now generates random colored squared

--- script.js
+++ script.js
@@ -5,23 +5,13 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     function createCheckerboard() {
         for (var i = 0; i < 63; i++) {
+            let randomColors = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
             let square = document.createElement("div");
-            // create red squares
-            if (i % 2 == 0) {
-                square.style.cssText = "background-color: red; width: 11.1%; float: left; padding-bottom: 11.1%";
-                square.textContent = i;
-                document.body.appendChild(square);
-            
-            // create black squares
-            } else {
-                square.style.cssText = "background-color: black; width: 11.1%; float: left; padding-bottom: 11.1%";
-                square.textContent = i;
-                document.body.appendChild(square);
-            }
-
+            // create random squares
+            square.style.cssText = `background-color: ${randomColors}; width: 11.1%; float: left; padding-bottom: 11.1%`;
+            square.textContent = i;
+            document.body.appendChild(square);
         }
     }
     createCheckerboard();
 });
-
- 
