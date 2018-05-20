0ca7b82384f65dfcca6d3a8b4a1e6efbc0222b46
Jake Lewis <jakeaustinlewis@gmail.com>
2018-05-20 16:03:03 -0500

    red and black checkerboard is created

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 // Your JS goes here
+
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    let x = document.createElement("div");
+    // x.textContent = 'We hope you enjoyed the ride.';
+    x.style.cssText = "background-color: red";
+
+    function createCheckerboard() {
+        for (var i = 0; i < 63; i++) {
+            let square = document.createElement("div");
+            if (i % 2 == 0) {
+                square.style.cssText = "background-color: red; width: 11.1%; float: left; padding-bottom: 11.1%";
+                square.textContent = i;
+                document.body.appendChild(square);
+                //remove all children and then repoluate
+            } else {
+                square.style.cssText = "background-color: black; width: 11.1%; float: left; padding-bottom: 11.1%";
+                square.textContent = i;
+                document.body.appendChild(square);
+            }
+
+        }
+    }
+    createCheckerboard();
+});
